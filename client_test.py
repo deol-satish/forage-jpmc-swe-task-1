6bf59038d61a5bb1f7d1a911fb6f6875785b3736
deol-satish <164171952+deol-satish@users.noreply.github.com>
2024-03-24 08:45:22 +0530

    Added assertion statements and unit tests for getRatio

--- client_test.py
+++ client_test.py
@@ -9,6 +9,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -16,6 +18,21 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+
+
+  """ ------------ Add more unit tests ------------ """
+
+  def test_getRatio(self):
+    prices_ratio = [{"price_a":30,"price_b":120},{"price_a":0,"price_b":120}]
+    """ ------------ Add the assertion below ------------ """
+    for prices in prices_ratio:
+      self.assertEqual(getRatio(prices["price_a"],prices["price_b"]),(prices["price_a"]/prices["price_b"]))
+    try:
+      self.assertEqual(getRatio(200,0),0)
+    except ZeroDivisionError:
+      print("Failed Test!Encountered ZeroDivision Error, Correct your method to include this testc case")
 
 
   """ ------------ Add more unit tests ------------ """
